8d866294b235ffa2af945e7c9294cceb999b7e4b
Petr Poupě <petr.poupe@gmail.com>
2014-06-20 14:37:49 +0200

    repair error page - deleting/repairing details

--- app/module/AdminModule/presenters/DashboardPresenter.php
+++ app/module/AdminModule/presenters/DashboardPresenter.php
@@ -11,24 +11,6 @@ use Tracy\Debugger as Debug;
 class DashboardPresenter extends BasePresenter
 {
 
-    /** @var \App\Model\Facade\TimeFacade @inject */
-    public $timeFacade;
-
-    /** @var \App\Model\Facade\ProjectFacade @inject */
-    public $projectFacade;
-
-    /** @var \App\Model\Facade\BookFacade @inject */
-    public $bookFacade;
-
-    /** @var \App\Model\Facade\AuthorFacade @inject */
-    public $authorFacade;
-
-    /** @var \App\Forms\AuthorFormFactory @inject */
-    public $authorFormFactory;
-
-    /** @var App\Model\Entity\Author */
-    private $author;
-
     protected function startup()
     {
         parent::startup();
@@ -37,62 +19,7 @@ class DashboardPresenter extends BasePresenter
 
     public function actionDefault()
     {
-
-//        $time = new \App\Model\Entity\Time();
-//        $time->setInterval(65);
-//        $this->timeFacade->save($time);
-//        $time = $this->timeFacade->find(4);
-//        Debug::barDump($time);
-//        $project = new \App\Model\Entity\Project;
-//        $project->setName("project Manhattan");
-//        $this->projectFacade->save($project);
-//        $project = $this->projectFacade->find(1);
-//        Debug::barDump($project);
-
-        $bookFacade = $this->bookFacade;
-        $authorFacade = $this->authorFacade;
-
-        $books = $bookFacade->findAll();
-        $authors = $authorFacade->findAll();
-
-        $this->author = $authorFacade->find(1);
-//	$author = new \App\Model\Entity\Author;
-//	$author->setFirstname("John");
-//	$author->setSurname("Doe");
-//	$authorFacade->save($author);
-
-        $book = $bookFacade->find(1);
-//	$book = new \App\Model\Entity\Book;
-//	$book->setAuthor($author);
-//	$book->setTitle("Lorem Ipsum");
-//	$book->setPublished(new \Nette\Utils\DateTime);
-//	$bookFacade->save($book);
-
-
-        $this->template->books = $books;
-        $this->template->count = count($books);
-
-//        Debug::barDump($books);
+        
     }
 
-// <editor-fold defaultstate="collapsed" desc="Forms">
-
-    public function createComponentAuthorForm()
-    {
-        $form = $this->formFactoryFactory
-                ->create($this->authorFormFactory)
-                ->setEntity($this->author)
-                ->create();
-        $form->onSuccess[] = $this->authorFormSuccess;
-        return $form;
-    }
-
-    public function authorFormSuccess($form)
-    {
-        $em = $this->formFactoryFactory->getEntityMapper();
-        $em->save($this->author, $form);
-        $em->load($this->author, $form);
-    }
-
-// </editor-fold>
 }
--- www/adminer/database.sql
+++ www/adminer/database.sql
@@ -63,11 +63,16 @@ CREATE TABLE `role` (
   UNIQUE KEY `UNIQ_57698A6A5E237E06` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+INSERT INTO `role` (`id`, `name`) VALUES
+(3,	'client'),
+(2,	'manager'),
+(1,	'programmer');
 
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `text_html` longtext COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -90,6 +95,8 @@ CREATE TABLE `user` (
   UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1,	'pupe.dupe@gmail.com',	'$2y$10$ws51c6qPg4M3nbnca65GKO3LGxViV/6l4lkQ8fNb.rxbLdN/fU6aW');
 
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
@@ -98,9 +105,12 @@ CREATE TABLE `user_role` (
   PRIMARY KEY (`user_id`,`role_id`),
   KEY `IDX_2DE8C6A3A76ED395` (`user_id`),
   KEY `IDX_2DE8C6A3D60322AC` (`role_id`),
-  CONSTRAINT `FK_2DE8C6A3D60322AC` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_2DE8C6A3A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  CONSTRAINT `FK_2DE8C6A3A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_2DE8C6A3D60322AC` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
+(1,	1),
+(1,	2);
 
--- 2014-06-18 08:50:51
+-- 2014-06-20 13:40:26
